258423ec41eba79c097600741084892c6208ef71
HH <haidarah134@gmail.com>
2021-08-22 11:38:49 +0700

    removing console log

--- src/pages/MazePage/Algorithm/traverse_moves.js
+++ src/pages/MazePage/Algorithm/traverse_moves.js
@@ -3,7 +3,6 @@ export const traverse_moves = (hash_map,from,to) =>{
     from = from.join(' ')
     to = to.join(' ')
     for(let key of Object.keys(hash_map).reverse()){
-        console.log(from,key,hash_map[key])
         if(hash_map[key].indexOf(from)>=0){
             from = key
             trail.unshift([...from.split(' '),'Trail'])
