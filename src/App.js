b3e8399eeea37caaec5f7e0dbe9a19a8a1a30e14
HH <haidarah134@gmail.com>
2021-08-22 11:38:04 +0700

    adding strict mode

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import './App.css';
 import { BrowserRouter as Router,Switch,Route } from 'react-router-dom'
 import Home from './pages'
 import MazePage from './pages/MazePage';
+import { StrictMode } from 'react';
 
 function App() {
   return (
+    <StrictMode>
     <Router>
       <Switch>
         <Route exact path='/' component={Home}/>
         <Route path='/maze' component={MazePage} exact/>
       </Switch>
     </Router>
+    </StrictMode>
   );
 }
 
